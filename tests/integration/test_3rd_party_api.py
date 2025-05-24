8162a469f10d5e222802ca7953b35641b7bf2b29
Vison Li <visonli@amazon.co.uk>
2025-05-24 18:15:19 +0100

    fix the import error

--- tests/integration/test_3rd_party_api.py
+++ tests/integration/test_3rd_party_api.py
@@ -1,13 +1,12 @@
 import pytest
 
-from app import court_client
-from helper import get_account_by_id
+from app import court_client, sm
 
 
 @pytest.mark.parametrize("account_id", [1, 2])
 def test_login(account_id):
     try:
-        account = get_account_by_id(account_id)
+        account = sm.get_account_by_id(account_id)
         court_client.login(account.username, account.password)
     except Exception as e:
         assert False, f"Unexpected exception raised: {e}"
