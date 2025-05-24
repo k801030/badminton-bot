17ea90cbcf5b46b84ce919e31e8ee17ec7cbb582
Vison Li <visonli@amazon.co.uk>
2025-05-24 16:58:44 +0100

    fix: use 120s to retry the reservation

--- src/helper.py
+++ src/helper.py
@@ -111,7 +111,7 @@ def select_court(courts: Courts, keyword) -> list[str]:
     return ids
 
 
-def book_court(client: Client, location, activity, date, start, end, keyword, max_duration_seconds=300,
+def book_court(client: Client, location, activity, date, start, end, keyword, max_duration_seconds=120,
                interval_seconds=1):
     """
     Tries to add a court booking for the specified time range and keyword.
