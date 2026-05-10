ae2aa022252aaa6d4d2c45d90cb162b266daa525
Vison Li <kk801030@gmail.com>
2026-05-10 23:36:29 +0800

    Switch from Secrets Manager to SSM Parameter Store for cost savings

--- app/secret_manager.py
+++ app/secret_manager.py
@@ -9,13 +9,16 @@ from models.line_secret import LineSecret
 class SecretManager:
     def __init__(self, region, is_dev=False):
         self.client = boto3.session.Session().client(
-            service_name="secretsmanager", region_name=region
+            service_name="ssm", region_name=region
         )
         self.is_dev = is_dev
 
     def _get_secret(self, secret_name):
-        response = self.client.get_secret_value(SecretId=secret_name)
-        return response["SecretString"]
+        response = self.client.get_parameter(
+            Name=secret_name,
+            WithDecryption=True
+        )
+        return response["Parameter"]["Value"]
 
     def get_line_secret(self) -> LineSecret:
         secret_name = "line_secret/dev" if self.is_dev else "line_secret"
