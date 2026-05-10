c49fbdeb8e3120eb9d6173fe992a32cea225c968
Vison Li <kk801030@gmail.com>
2026-05-11 00:16:34 +0800

    refactor: condense sample_request formatting in main.py

--- app/main.py
+++ app/main.py
@@ -7,24 +7,17 @@ from datetime_utils import now
 from models.court_booking_request import CourtBookingRequest
 
 sample_request = {
-  "accountId": "1",
-  "location": "queensbridge-sports-community-centre",
-  "activity": "badminton-40min",
-  "keyword": "Court 1,Court 3",
-  "day_offset": 7,
-  "slots": [{
-    "start_time": "19:20",
-    "end_time": "20:00"
-  }, {
-    "start_time": "20:00",
-    "end_time": "20:40"
-  }, {
-    "start_time": "20:40",
-    "end_time": "21:20"
-  }, {
-    "start_time": "21:20",
-    "end_time": "22:00"
-  }]
+    "accountId": "1",
+    "location": "queensbridge-sports-community-centre",
+    "activity": "badminton-40min",
+    "keyword": "Court 1,Court 3",
+    "day_offset": 7,
+    "slots": [
+        {"start_time": "19:20", "end_time": "20:00"},
+        {"start_time": "20:00", "end_time": "20:40"},
+        {"start_time": "20:40", "end_time": "21:20"},
+        {"start_time": "21:20", "end_time": "22:00"},
+    ],
 }
 
 if __name__ == "__main__":
